bf2bbffcac0617802e8b7a1458eb43d1eff01ad5
Anderson Begossi <andersonbegossi@gmail.com>
2023-08-02 20:10:30 -0300

    feat: install and use rtn-calculator

--- App.tsx
+++ App.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import RTNCalculator from 'rtn-calculator/js/NativeCalculator';
 
 function App(): JSX.Element {
+  const [result, setResult] = useState<number | null>(null);
+
   const isDarkMode = useColorScheme() === 'dark';
+
+  const add = async () => {
+    const value = await RTNCalculator?.add(3, 7);
+
+    if (value) {
+      setResult(value);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
@@ -21,7 +33,10 @@ function App(): JSX.Element {
       />
       <ScrollView contentContainerStyle={styles.scrollViewContentContainer}>
         <View style={styles.container}>
-          <Text>Hello World</Text>
+          <TouchableOpacity style={styles.button} onPress={add}>
+            <Text>Calcular</Text>
+          </TouchableOpacity>
+          {result && <Text>{result}</Text>}
         </View>
       </ScrollView>
     </SafeAreaView>
